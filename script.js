fc325e1128e381d8687982a11f5ac6dfe26ead80
Helen Jiang <helenjiang@Helens-iMac.local>
2018-03-30 12:06:02 -0700

    added Discover Popular Movies to the front page and show 10 results per page for genre search

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const MDBAPIKey = 'ba52c91a2a4420e8af20bc1f8d3ae86f';
 const YoutubeAPIKey = 'AIzaSyCp3p6J0vMOVgB4xtInEszElwMrR-Yq5JY';
 const YoutubeURL = 'https://www.googleapis.com/youtube/v3/search';
+const MDBAURL = 'https://api.themoviedb.org/3/discover/movie?api_key=ba52c91a2a4420e8af20bc1f8d3ae86f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false';
+const STORE = {
+    //MDBResults: [],
+    page: 1,
+    totalPages: null,
+   //fetchNextPage: false,
+    genre: null
+};
 
 
+/**************************************************************************************************  
+When page loads, function automatically searchs MDBAPI for current popular movies
+success calls fucntion displayPopular
+****************************************************************************************************/
+function searchPopular() {
+    const settings = {
+        url: MDBAURL,
+        success: displayPopular
+    }
+    $.ajax(settings);
+}
+
+/**************************************************************************************************  
+displays the popular movies from MDBAPI
+****************************************************************************************************/
+function displayPopular(data) {
+    const popular = data.results.map(function (item){
+        return movieHTML(item);
+    });
+    
+    $('.js-popular-movies').html(popular);
+}
 
 /**************************************************************************************************  
 HTML to add the clips info from Youtube
@@ -62,7 +92,7 @@ function handleClips() {
 
     $(document).on('click', '.watch-clip', function (event) {
         event.preventDefault();
-        const title = $('.title-search').val();
+        const title = $(this).val();
         searchYoutube(title);
 
     });
@@ -76,11 +106,11 @@ function handleClips() {
 function movieHTML(item) {
 
     return `
-        <div> 
-            <option class="title-search" value="${item.title}">Title: ${item.title} </option>
+        <div class="movie"> 
+            <option class="title-search" ">${item.title} </option>
             <p>Release Date: ${item.release_date}</p>
             <p>Summary: ${item.overview}</p>
-            <button class="watch-clip"> See Clips</button>
+            <button class="watch-clip" value="${item.title}"> Search Youtube</button>
         </div>
     
     `
@@ -92,12 +122,25 @@ function movieHTML(item) {
  * calls addMovieHTML to display movies on page
  ***************************************************************/
 function displayMovies(data) {
+    STORE.MDBResults = data.results;
+    STORE.page = data.page;
+    STORE.totalPages = data.total_pages;
+    STORE.fetchNextPage = false;
 
-    const result = data.results.map(function (item) {
+    const result = data.results.map(function (item, index) {
+        console.log(index);
+        if(index > 9) {
+            return;
+        }
         return movieHTML(item);
     });
-
+    
+    $('.movies').prop('hidden', false);
+    $('.js-results').prop('hidden', false);
     $('.js-results').html(result);
+    if(STORE.MDBResults.length > 10) {
+        $('.js-results').append('<button class="next"> Next </button>');
+    }
 
 }
 
@@ -105,10 +148,10 @@ function displayMovies(data) {
 /********************************************************* 
 Search MDBA API for movies of a specific genre
 ***********************************************************/
-function searchMovieAPI(genre) {
+function searchMovieAPI() {
 
     const settings = {
-        url: `https://api.themoviedb.org/3/genre/${genre}/movies?api_key=${MDBAPIKey}&language=en-US&include_adult=false&sort_by=created_at.asc`,
+        url: `https://api.themoviedb.org/3/genre/${STORE.genre}/movies?api_key=${MDBAPIKey}&language=en-US&include_adult=false&sort_by=created_at.asc&page=${STORE.page}`,
         success: displayMovies
     }
     $.ajax(settings);
@@ -125,10 +168,44 @@ function handleSearch() {
     $(document).on('click', '.search', function (event) {
         event.preventDefault();
         let genre = $('#search-by-genre').val();
-        console.log(genre);
-        searchMovieAPI(genre);
-
+        STORE.genre = genre;
+        searchMovieAPI();
+        
+        
     });
+
+}
+
+function renderPage (nextResults) {
+
+    const pageHTML = nextResults.map(function (item, index) {
+        return movieHTML(item);
+    });
+    
+    $('.js-results').html(pageHTML);
+   if(STORE.MDBResults.length > 10) {
+        $('.js-results').append('<button class="next"> Next </button>');
+    }
+
+}
+
+function handleNext () {
+
+    $(document).on('click', '.next', function (event) {
+        event.preventDefault();
+        //alert('next button');
+        if(!STORE.fetchNextPage) {
+            const nextResults = STORE.MDBResults.slice(10);
+            renderPage(nextResults);
+            STORE.fetchNextPage = true;
+        }
+        else {
+            if (STORE.page < STORE.totalPages) {
+                STORE.page++;
+                searchMovieAPI();
+            }
+        }
+    })
 
 }
 
@@ -137,12 +214,12 @@ function handleSearch() {
 function to call event listeners 
 **********************************************************************************/
 function handleClicks() {
-
+    searchPopular();
     handleSearch();
     handleClips();
-
-    
+    handleNext();
 }
 
 /*Call handleClicks when Document ready*/
 $(handleClicks);
+
